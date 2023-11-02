fe6c94e47388fe03b8cbb8f6aa5c96c2cd483ae2
99winnmin <ojysep9987@gmail.com>
2023-11-02 23:54:43 +0900

    :zap: Fix : WebFlux -> Coroutine 으로 변경

--- src/main/kotlin/net/chandol/sample/coroutine/api/client/SampleRestClient.kt
+++ src/main/kotlin/net/chandol/sample/coroutine/api/client/SampleRestClient.kt
@@ -1,5 +1,6 @@
 package net.chandol.sample.coroutine.api.client
 
+import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
@@ -19,5 +20,19 @@ class SampleRestClient {
             .exchangeToMono {
                 it.bodyToMono(String::class.java)
             }
+    }
+
+    // Mono 를 최대한 숨김 ( 코루틴을 사용한다고 하면 )
+    suspend fun sampleGet2(param: String): String {
+        return client.get()
+            .uri { uriBuilder ->
+                uriBuilder.path("/get")
+                    .queryParam("param", param)
+                    .build()
+            }
+            .exchangeToMono {
+                it.bodyToMono(String::class.java)
+            }
+            .awaitSingle()
     }
 }
--- src/main/kotlin/net/chandol/sample/coroutine/api/endpoint/SampleController.kt
+++ src/main/kotlin/net/chandol/sample/coroutine/api/endpoint/SampleController.kt
@@ -1,5 +1,6 @@
 package net.chandol.sample.coroutine.api.endpoint
 
+import kotlinx.coroutines.reactor.awaitSingle
 import mu.KotlinLogging
 import net.chandol.sample.coroutine.api.client.SampleRestClient
 import org.springframework.http.ResponseEntity
@@ -23,6 +24,18 @@ class SampleController(
             .flatMap { sampleRestClient.sampleGet(param) }
             .map { ResponseEntity.ok(it) }
             .doOnNext { log.info { "요청 완료!... ${it.body}" } }
+    }
+
+    // 위에꺼보다 가독성이 훨씬 좋음
+    @GetMapping("/hello2")
+    suspend fun hello2(@RequestParam param: String): ResponseEntity<String> {
+        log.info { "요청 받음... $param" }
+        // awaitSingle()을 통해 coroutine 으로 처리하고 비동기로 응답한다.
+        val response = sampleRestClient.sampleGet2(param)
+//        val response = sampleRestClient.sampleGet(param).block() // 에러남
+        log.info { "요청 완료!... ${response}" }
+
+        return ResponseEntity.ok(response)
     }
 
     companion object {
